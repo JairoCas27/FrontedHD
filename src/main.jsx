da420367c58292b5c5b66dc337ae38bfbd37cc1a
JairoCas27 <u23200267@utp.edu.pe>
2026-04-25 21:39:59 -0500

    Implementacion de importacion de servicios

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom' // 1. Importaciones del enrutador
+import { BrowserRouter, Routes, Route } from 'react-router-dom' 
 import './index.css'
 import App from './App.jsx'
-import Nosotros from './Nosotros.jsx' // 2. Importación de tu nueva vista
+import Nosotros from './Nosotros.jsx' 
+import Servicios from './Servicios.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
         {/* 3. Configuración de las rutas */}
         <Route path="/" element={<App />} />
         <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/servicios" element={<Servicios />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
