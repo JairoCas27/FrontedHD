c82407919c4bdf119f216643e6daa240ff36df44
Cristian Candela <cristiancandela561@gmail.com>
2026-04-27 23:35:28 -0500

    feat: estructura inicial de la vista perfil propietario

--- src/pages/propietario/PerfilPropietario.jsx
+++ src/pages/propietario/PerfilPropietario.jsx
@@ -1,13 +1,26 @@
 import { FiUser } from "react-icons/fi"
+import { useState } from "react";
+import { User } from "lucide-react";
 
 export default function PerfilPropietario() {
+
+  const [usuario] = useState({
+    nombre: "Juan Pérez",
+    email: "juan@email.com",
+    condominio: "Condominio Los Pinos",
+    torre: "Torre A",
+    piso: "Piso 3",
+    departamento: "Dpto 302",
+    rol: "Propietario"
+  });
+  
   return (
     <div style={{ padding: "1rem" }}>
       <div style={{ marginBottom: "2rem" }}>
         <h1 style={{ fontSize: "1.8rem", color: "#1e293b" }}>Mi Perfil</h1>
         <p style={{ color: "#64748b" }}>Información personal del propietario</p>
       </div>
-
+    
       <div style={{
         maxWidth: "500px",
         background: "linear-gradient(135deg, #f97316, #ea580c)",
@@ -15,9 +28,15 @@ export default function PerfilPropietario() {
         padding: "2rem",
         color: "white"
       }}>
-        <FiUser size={40} />
-        <h2>Nombre Usuario</h2>
-        <p>Propietario</p>
+        <div style={{ padding: "1rem" }}>
+      <h1>Mi Perfil</h1>
+
+      <div>
+        <User size={40} />
+        <h2>{usuario.nombre}</h2>
+        <p>{usuario.rol}</p>
+      </div>
+    </div>
       </div>
     </div>
   );
