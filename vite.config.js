534f83bf4b52697a7e5f665a96e94a39f351f1be
JairoCas27 <u23200267@utp.edu.pe>
2026-07-18 11:35:23 -0500

    Mejoras implementadas

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,14 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build', 
-  }
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  },
 })
